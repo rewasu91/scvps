955a2dcaa72a76da022189f99a71eda53b400cf7
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-08-31 19:23:10 +0800

    Update trialtrojan.sh

--- Menu/trojan/trialtrojan.sh
+++ Menu/trojan/trialtrojan.sh
@@ -170,7 +170,7 @@ Username="$(echo ${Username} | sed 's/ //g' | tr -d '\r' | tr -d '\r\n' )"; # > 
 # // Validate Input
 if [[ $Username == "" ]]; then
     clear;
-    echo -e "${EROR} Please Input an Username !";
+    echo -e "${EROR} Sila masukkan Username !";
     exit 1;
 fi
 
@@ -182,7 +182,7 @@ if [[ "$( cat /etc/xray-mini/client.conf | grep -w ${Username})" == "" ]]; then
     Do=Nothing;
 else
     clear;
-    echo -e "${ERROR} User ( ${YELLOW}$Username${NC} ) Already Exists !";
+    echo -e "${ERROR} Username ( ${YELLOW}$Username${NC} ) sudah dipakai !";
     exit 1;
 fi
 
@@ -207,7 +207,7 @@ nontls_port=$( cat /etc/xray-mini/nontls.json | grep -w port | awk '{print $2}' 
 export CHK=$( cat /etc/xray-mini/tls.json );
 if [[ $CHK == "" ]]; then
     clear;
-    echo -e "${ERROR} Your VPS Crash, Contact admin for fix it";
+    echo -e "${ERROR} Terdapat masalah teknikal didalam VPS anda. Sila hubungi admin untuk fixkan VPS anda !";
     exit 1;
 fi
 
