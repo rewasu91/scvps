ee79ac859b04b9cb57042c9226fe94ca5bca3dd1
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-09-03 00:16:42 +0800

    Update wg-set.sh

--- Setup/wg-set.sh
+++ Setup/wg-set.sh
@@ -170,7 +170,7 @@ if [[ $OS == "ubuntu" ]]; then
 	apt autoremove -y;
 	apt install wireguard -y;
 elif [[ $OS == "debian" ]]; then
-	echo "deb http://deb.debian.org/debian/ unstable main" >/etc/apt/sources.list.d/unstable.list
+	echo 'deb http://ftp.debian.org/debian buster-backports main' | sudo tee /etc/apt/sources.list.d/buster-backports.list
 	printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' >/etc/apt/preferences.d/limit-unstable
 	apt update -y;
 	apt upgrade -y;
