78fef89c709d3a03dd7b822cac932ad0f979720e
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-09-03 15:38:53 +0800

    Update trojan-kill.sh

--- Menu/Pro/Autokill/trojan-kill.sh
+++ Menu/Pro/Autokill/trojan-kill.sh
@@ -178,12 +178,15 @@ fi
 # // Start
 # ════════
 clear;
-echo "";
+echo -e "";
+echo -e "";
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU.";
+echo -e "";
 echo -e "${CYAN}══════════════════════════════════════════${NC}";
 echo -e "${WBBG}       [ Setting Trojan Autokill ]        ${NC}";
 echo -e "${CYAN}══════════════════════════════════════════${NC}";
 echo -e "";
-echo "$(date) Autokill Trojan Multilogin Versi 1.0 Stable.";
+echo "$(date) Autokill Trojan Multilogin Versi 2.0 (Multiport).";
 echo "$(date) Dipasang oleh KaizenVPN.";
 echo "$(date) Memulakan Servis Trojan Autokill.";
 
