6cc659eca201e221fc7fbd88b247c847b91cbeeb
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-09-03 14:56:11 +0800

    Update menu.sh

--- Menu/vmess/menu.sh
+++ Menu/vmess/menu.sh
@@ -159,10 +159,10 @@ clear;
 # // Menu Vmess
 # ═════════════
 clear;
-echo -e ""
-echo -e ""
-cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
-echo -e ""
+echo -e "";
+echo -e "";
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU.";
+echo -e "";
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
 echo -e "${WBBG}               [ Menu Vmess ]               ${NC}";
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
