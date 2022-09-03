e50865caae4fad89070422228b5ef8b680120939
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-09-03 14:47:53 +0800

    Update vmessexp.sh

--- Menu/vmess/vmessexp.sh
+++ Menu/vmess/vmessexp.sh
@@ -186,13 +186,17 @@ if [[ $sisa_hari -lt 1 ]]; then
     systemctl restart xray-mini@nontls;
 
     # // Successfull Deleted Expired Client
-    echo "";
+    clear;
+    echo -e ""
+    echo -e ""
+    cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+    echo -e ""
     echo -e "${CYAN}════════════════════════════════════════════${NC}";
     echo -e "${WBBG}       [ Memadam Akaun Expire Vmess ]       ${NC}";
     echo -e "${CYAN}════════════════════════════════════════════${NC}";
     echo -e "";
-    echo "Username : $username | Expire Pada : $expired | Dipadam Pada : $now" >> /etc/kaizenvpn/vmess-expired-deleted.db;
-    echo "Username : $username | Expire Pada : $expired | Dipadam Pada : $now";
+    echo "  Username : $username | Expire Pada : $expired | Dipadam Pada : $now" >> /etc/kaizenvpn/vmess-expired-deleted.db;
+    echo "  Username : $username | Expire Pada : $expired | Dipadam Pada : $now";
 
 else
     Skip="true";
