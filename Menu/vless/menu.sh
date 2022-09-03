f03d922e2548f7b7e848eeb601a53ef509aa331d
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-09-03 14:51:29 +0800

    Update menu.sh

--- Menu/vless/menu.sh
+++ Menu/vless/menu.sh
@@ -159,7 +159,10 @@ clear;
 # // Menu Vless
 # ═════════════
 clear;
-echo "";
+echo -e ""
+echo -e ""
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+echo -e ""
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
 echo -e "${WBBG}               [ Menu Vless ]               ${NC}";
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
@@ -174,9 +177,10 @@ echo -e " ${GREEN}[ 06 ]${NC} ► Link config setiap akaun";
 echo -e " ${GREEN}[ 07 ]${NC} ► Memadam akaun yang sudah expire";
 echo -e " ${GREEN}[ 08 ]${NC} ► Senarai akaun yang sedang login";
 echo -e " ${GREEN}[ 09 ]${NC} ► Log akses akaun";
+echo -e " ${GREEN}[ 10 ]${NC} ► Kembali ke menu utama";
 echo "";
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
-read -p "► Sila masukkan nombor pilihan anda [1-9] : " selection_mu;
+read -p "► Sila masukkan nombor pilihan anda [1-10] : " selection_mu;
 
 case $selection_mu in
     1)
@@ -205,6 +209,9 @@ case $selection_mu in
     ;;
     9)
         vlesslog;
+    ;;
+    10)
+       menu;
     ;;
     *)
         echo -e "${ERROR} Sila masukkan nombor yang betul!";
