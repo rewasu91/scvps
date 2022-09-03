4e856964312ee81305b4daa4f5e03fe48396460b
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-09-03 14:43:11 +0800

    Update delvmess.sh

--- Menu/vmess/delvmess.sh
+++ Menu/vmess/delvmess.sh
@@ -158,8 +158,10 @@ clear;
 # ════════════════
 # // Memadam Akaun 
 # ════════════════
-clear;
-echo "";
+echo -e ""
+echo -e ""
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+echo -e ""
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
 echo -e "${WBBG}         [ Memadam Akaun Vmess ]            ${NC}";
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
@@ -189,9 +191,12 @@ sed -i "/\b$client\b/d" /etc/xray-mini/client.conf;
 systemctl restart xray-mini@tls;
 systemctl restart xray-mini@nontls;
 clear;
-echo "";
+echo -e ""
+echo -e ""
+cowsay -f ghostbusters "SELAMAT DATANG BOSKU."
+echo -e ""
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
 echo -e "${WBBG}         [ Memadam Akaun Vmess ]            ${NC}";
 echo -e "${CYAN}════════════════════════════════════════════${NC}";
 echo -e "";
-echo -e "${OKEY} Username ( ${YELLOW}$client${NC} ) telah dipadam !";
+echo -e "  ${OKEY} Username ( ${YELLOW}$client${NC} ) telah dipadam !";
