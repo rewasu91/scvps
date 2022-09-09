da1a9fb71e4132484aa3a189d1855256f6f11aef
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-09-10 03:24:32 +0800

    Add files via upload

---
+++ Menu/other/autoexp.sh
@@ -0,0 +1,19 @@
+chmod +x /usr/local/sbin/sshexp
+chmod +x /usr/local/sbin/vmessexp
+chmod +x /usr/local/sbin/vlessexp
+chmod +x /usr/local/sbin/ssexp
+chmod +x /usr/local/sbin/wgexp
+chmod +x /usr/local/sbin/trojanexp
+chmod +x /usr/local/sbin/socksexp
+chmod +x /usr/local/sbin/httpexp
+chmod +x /usr/local/sbin/ssrexp
+/usr/local/sbin/sshexp
+/usr/local/sbin/vmessexp
+/usr/local/sbin/vlessexp
+/usr/local/sbin/ssexp
+/usr/local/sbin/wgexp
+/usr/local/sbin/trojanexp
+/usr/local/sbin/socksexp
+/usr/local/sbin/httpexp
+/usr/local/sbin/ssrexp
+echo "$(date) | Berjaya mengaktifkan Auto Expired" > /etc/kaizenvpn/autoexp.log
