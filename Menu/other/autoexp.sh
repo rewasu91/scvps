eaf20e77d48b2ea781d49a47af70a617c354499f
Kaizen <95635242+rewasu91@users.noreply.github.com>
2022-11-23 00:31:57 +0800

    Update autoexp.sh

--- Menu/other/autoexp.sh
+++ Menu/other/autoexp.sh
@@ -1,10 +1,10 @@
-/usr/local/sbin/sshexp
-/usr/local/sbin/vmessexp
-/usr/local/sbin/vlessexp
-/usr/local/sbin/ssexp
-/usr/local/sbin/wgexp
-/usr/local/sbin/trojanexp
-/usr/local/sbin/socksexp
-/usr/local/sbin/httpexp
-/usr/local/sbin/ssrexp
+sshexp
+vmessexp
+vlessexp
+ssexp
+wgexp
+trojanexp
+socksexp
+httpexp
+ssrexp
 echo "$(date) | Berjaya mengaktifkan Auto Expired" > /etc/kaizenvpn/autoexp.log
